a91ffb71a3a8511a8ece40011ef7925da8e34204
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-28 13:25:50 +0100

    patch(anybar): anybar to use polybar bar instead of dwm bar

--- config.def.h
+++ config.def.h
@@ -8,6 +8,9 @@ static const unsigned int borderpx  = 6;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *altbarcmd        = "$HOME/.config/polybar/launch.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "Meslo LGM Nerd Font:size=13" };
 static const char dmenufont[]       = "Meslo LGM Nerd Font:size=13";
 static const char col_gray1[]       = "#222222";
