234217d1655183d72259498dc2b0c8bb23e3310f
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-04-29 11:08:30 +0200

    feat(center floating windows): Floating windows are always centered
           Floating windows are centered https://dwm.suckless.org/patches/alwayscenter/

--- dwm.c
+++ dwm.c
@@ -1148,6 +1148,9 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+    // Floating windows are centered https://dwm.suckless.org/patches/alwayscenter/
+    c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) /2 ;
+    c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) /2 ;
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
