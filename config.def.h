ddd0cacd079a37e77d5d88aab53510d72a406a8f
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-05-13 11:43:41 +0200

    orgcapture!!

--- config.def.h
+++ config.def.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "emacs",  NULL,       "Org Select", 0,       1,           -1 },
+	{ "orgcapture",  NULL,       NULL, 0,       1,           -1 },
 };
 
 /* layout(s) */
