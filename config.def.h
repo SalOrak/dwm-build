d94babfbbc6d61f20a657cb743a9be7a7cce4ee7
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-05-13 11:11:24 +0200

    feat(qutebrowser): added qutebrowser

--- config.def.h
+++ config.def.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Emacs",  NULL,       "*Org Select*", 0,       1,           -1 },
 };
 
 /* layout(s) */
@@ -74,7 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofimenucmd[] = { "rofi", "-show", "drun", "dmenu"};
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL};
 static const char *editorcmd[] = {"emacsclient", "--create-frame", "--alternate-editor=''"};
 static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 
@@ -97,7 +98,7 @@ static const char *showlastnotification[]  = { "dunstctl", "history-pop", NULL};
 static const char *emacsorgcapture[]  = { "emacsclient", "-e","(sk-window-popup-org-capture)", NULL};
 
 /* Browser */
-static const char *browser[]  = { "firefox", "--browser", NULL};
+static const char *browser[]  = { "qutebrowser", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
